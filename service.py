5b793e585caa5fc5e5a972cdaecfa45beb697bcf
Wanderson <wanderson.coord@gmail.com>
2024-09-13 11:48:57 -0300

    Add validate of body using regex and format application stdout

--- service.py
+++ service.py
@@ -4,6 +4,7 @@ import requests
 import datetime, time
 import yaml
 import os
+import re
 
 load_dotenv()
 
@@ -16,15 +17,32 @@ c = Gauge('uptime_requests_response_time', 'HTTP Request', [
     'service_name',
     'method', 
     'uri', 
-    'status_code'
+    'status_code',
+    'type_validate'
 ])
+
+def validate_body(type_validate, body, body_to_validate):
+    if type_validate == "BODY_REGEX":
+        x = re.search(body_to_validate, body)
+        try:
+            x.span()
+            return True	
+        except:
+            return False
+    
+    if type_validate == "BODY_EQUAL":
+        return body == body_to_validate
+    
+def validate_status_code(status_code):
+    return status_code >= 200 < 400
+
 
 def process_request():
     SLEEP_TIME_SECOND = int(os.getenv("SLEEP_TIME_SECOND", "15"))
     
     data = get_payload()
     for item in data:
-
+        response_validated = True
         t_start = datetime.datetime.now()
 
         url = item["url"]
@@ -39,20 +57,31 @@ def process_request():
                 response = requests.put(url, data=payload, headers=headers)
             else: 
                 response = requests.get(url, data=payload, headers=headers)
+            
+            if "validate_by_body" in item:
+                response_validated = validate_body(item['validate_by_body']['type'], response.text, item['validate_by_body']['body_text'])
+            else:
+                response_validated = validate_status_code(response.status_code)
 
-            print(f"SUCCESS => SLEEP_TIME_SECOND: {SLEEP_TIME_SECOND}, SERVICE_NAME: {item['service_name']}, METHOD: {item['method']}, STATUS: {response.status_code}")
+            print(f"{datetime.datetime.now()} INFO => SLEEP_TIME_SECOND: {SLEEP_TIME_SECOND}, SERVICE_NAME: {item['service_name']}, METHOD: {item['method']}, STATUS: {response.status_code}")
             status = f"{response.status_code}"
             total_seconds = response.elapsed.total_seconds()
 
         except requests.exceptions.RequestException as e:
             t_end = datetime.datetime.now()
+            response_validated = False
             duration = t_end - t_start
             total_seconds = round(duration.total_seconds())
             status = f"500"
-            print(f"ERROR => SLEEP_TIME_SECOND: {SLEEP_TIME_SECOND}, SERVICE_NAME: {item['service_name']}, METHOD: {item['method']}, STATUS: {500}")
+            print(f"{datetime.datetime.now()} ERROR => SLEEP_TIME_SECOND: {SLEEP_TIME_SECOND}, SERVICE_NAME: {item['service_name']}, METHOD: {item['method']}, STATUS: {status}")
 
-
-        c.labels(item['service_name'], item['method'], url, status).set(total_seconds)
+        c.labels(
+            item['service_name'], 
+            item['method'], 
+            url, 
+            status, 
+            "success" if response_validated else "error"
+        ).set(total_seconds)
     
     time.sleep(SLEEP_TIME_SECOND)
 
